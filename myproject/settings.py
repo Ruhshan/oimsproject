6caba911104faf9fca3af74acac9a2445c46f58b
ruhshan <ruhshan.ahmed@gmail.com>
2016-12-08 01:14:15 +0600

    additional files and changes for Heroku

--- myproject/settings.py
+++ myproject/settings.py
@@ -153,3 +153,18 @@ STATICFILES_DIRS = [
 # SESSION_EXPIRE_AT_BROWSER_CLOSE=True
 # SESSION_SECURITY_WARN_AFTER=60
 # SESSION_SECURITY_EXPIRE_AFTER=70
+
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
