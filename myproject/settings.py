0eb90b5580b3a6479fd0491ef44d7dd2351c382f
ruhshan <ruhshan.ahmed@gmail.com>
2016-12-08 14:05:01 +0600

    uploading manual

--- myproject/settings.py
+++ myproject/settings.py
@@ -145,11 +145,11 @@ LOGIN_REDIRECT_URL = 'home'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     '/var/www/static/',
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # SESSION_EXPIRE_AT_BROWSER_CLOSE=True
 # SESSION_SECURITY_WARN_AFTER=60
 # SESSION_SECURITY_EXPIRE_AFTER=70
